a9f529c04061191ccf4fc7c128917d96f87b7c53
yuliiabalenko <yuliiabalenko@gmail.com>
2020-07-14 10:53:04 +0300

    ToolTip task

--- src/test/java/hello_webdriver/DragAndDrop.java
+++ src/test/java/hello_webdriver/DragAndDrop.java
@@ -5,23 +5,50 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DragAndDrop {
-    @Test
-    public void DradnDrop() throws InterruptedException {
+    String elements = "One\nTwo\nThree\nFour\nFive\nSix";
+    WebDriver driver;
 
-        WebDriver driver = new ChromeDriver();
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
         String URL = "http://demoqa.com/sortable/";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(URL);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkSortingOfElements() {
+        List<WebElement> actualElements = driver.findElements(By.xpath("//*[@id='demo-tabpane-list']/div"));
+        for(WebElement ele:actualElements) {
+            String element_name = ele.getText();
+            Assert.assertEquals(element_name, elements);
+        }
+    }
+
+    @Test
+    public void dragAndDrop() {
 
         WebElement From = driver.findElement(By.xpath("//*[@class='vertical-list-container mt-4']/div[2]"));
         WebElement To = driver.findElement(By.xpath("//*[@class='vertical-list-container mt-4']/div[5]"));
 
         Actions act = new Actions(driver);
         act.dragAndDrop(From, To).build().perform();
-        Thread.sleep(2000);
-        driver.quit();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
     }
 
 }
